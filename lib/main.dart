a92f221e9e6099fc857e3191c636099af093f03d
baotan2000 <lamphucbaotan@gmail.com>
2023-09-19 15:59:30 +0700

    form home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hello_word/home_page.dart';
 
 void main(List<String> args) {
   runApp(const MaterialApp(
@@ -14,9 +15,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Flutter Demo",
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: HomePage(),
     );
   }
 }
