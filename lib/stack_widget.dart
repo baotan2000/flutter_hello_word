7c772f96f34b04a0165989e238956d85e6aacd0e
baotan2000 <lamphucbaotan@gmail.com>
2023-09-20 15:39:31 +0700

    clip behavior vs decoration

--- lib/stack_widget.dart
+++ lib/stack_widget.dart
@@ -11,30 +11,33 @@ class _StackWidgetState extends State<StackWidget> {
   @override
   Widget build(BuildContext context) {
     return Stack(
+      alignment: Alignment.center,
       children: [
-        Align(
-          alignment: Alignment.centerLeft,
-          child: Container(
-            width: 100,
-            height: 100,
-            color: Colors.red,
-          ),
-        ),
-        Align(
-          alignment: Alignment.topRight,
-          child: Container(
-            height: 100,
-            width: 100,
-            color: Colors.blue,
-          ),
-        ),
-        Positioned(
-          left: 10,
-          top: 10,
-          child: Container(
-            width: 100,
-            height: 100,
-            color: Colors.red,
+        Container(
+          decoration: BoxDecoration(
+              border: Border.all(), borderRadius: BorderRadius.circular(20)),
+          height: 200,
+          width: 200,
+          child: Stack(
+            clipBehavior: Clip.none,
+            children: [
+              Positioned(
+                bottom: 0,
+                child: Container(
+                  width: 70,
+                  height: 300,
+                  color: Colors.red,
+                ),
+              ),
+              Positioned(
+                right: -20,
+                child: Container(
+                  width: 70,
+                  height: 70,
+                  color: Colors.blue,
+                ),
+              ),
+            ],
           ),
         )
       ],
