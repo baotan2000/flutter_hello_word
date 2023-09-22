c03828654813eaf21a952c360e975aacc7754dd6
baotan2000 <lamphucbaotan@gmail.com>
2023-09-22 15:15:11 +0700

    setup build context

---
+++ lib/demo_build_context_widget.dart
@@ -0,0 +1,51 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+
+class DemoBuildContextWidget extends StatefulWidget {
+  String name = "Demo build context";
+
+  @override
+  State<DemoBuildContextWidget> createState() => _DemoBuildContextWidgetState();
+}
+
+class _DemoBuildContextWidgetState extends State<DemoBuildContextWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return ParentWidget(
+      child: ChildrenWidget(),
+    );
+  }
+}
+
+class ParentWidget extends StatefulWidget {
+  Widget child;
+
+  ParentWidget({
+    Key? key,
+    required this.child,
+  }) : super(key: key);
+
+  @override
+  State<ParentWidget> createState() => _ParentWidgetState();
+}
+
+class _ParentWidgetState extends State<ParentWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text("Parent widget"),
+        widget.child,
+      ],
+    );
+  }
+}
+
+class ChildrenWidget extends StatelessWidget {
+  const ChildrenWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("Children widget");
+  }
+}
